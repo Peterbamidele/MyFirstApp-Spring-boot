3c7c3815108595933eeb7fe41b522924f01641ad
Peterbamidele <peterbamidele@gmail.com>
2021-03-28 01:32:27 +0100

    first commit

---
+++ src/test/java/com/devclusaka/myBlog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devclusaka.myBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
